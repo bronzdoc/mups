96914ca0e618a16d31b9393bfc32a7f592a43157
bronzdoc <lsagastume1990@gmail.com>
2016-06-26 20:44:45 -0600

    Removed raw socket to connect to the api and use mtime if available to start the stream

--- lib/mups/consumer.rb
+++ lib/mups/consumer.rb
@@ -2,39 +2,62 @@ require "json"
 require "thread"
 require "socket"
 require "logger"
+require "redis"
 require "pry"
+require "http"
 
 module Mups
   class Consumer
-    attr_reader :queue
+    attr_reader :queue, :uri
 
-    def initialize
-      uri = URI("http://stream.meetup.com")
+    def initialize(redis_url)
+      @redis = Redis.new(url: URI.parse(redis_url))
+
+      # Try to start the stream with the las mtime enqueued
+      @uri = if @redis.llen("mups:mtime") > 0
+               URI("http://stream.meetup.com/2/open_events")
+             else
+               mtime = @redis.llget("mups:mtime")
+               URI("http://stream.meetup.com/2/open_events?since_mtime=#{mtime}")
+             end
       @queue = Queue.new
-      @socket = TCPSocket.new(uri.host, uri.port)
       @logger = Logger.new(STDOUT)
     end
 
     def start
+      Thread.abort_on_exception = true
+
       read = Thread.new(queue) do |queue|
-        @socket.puts "GET /2/open_events HTTP/1.1\r\n"
-        @socket.puts "Host: stream.meetup.com"
-        @socket.puts "\r\n"
-        while data = @socket.gets.strip
-          if data.chars.first == "{"
-            parsed_data = JSON.parse(data)
-            queue.push(parsed_data)
+        body = HTTP.get(@uri).body
+
+        buffer = ""
+        body.each do |chunk|
+          buffer += chunk
+          # Enqueue event data just if we have a complete chunk
+          if buffer.chars.last == "\n"
+            queue.push(buffer)
+            buffer = ""
           end
         end
-        @socket.close
-        queue.push NilClass
+        # Push nil to signify there's no more data in the queue
+        queue.push nil
       end
 
       consume = Thread.new(queue) do |queue|
         while queue_data = queue.pop
           break if queue_data.is_a? NilClass
-          parsed_data = JSON.parse(queue_data)
-          @logger.info("Queuing -> Title: #{queue_data["name"]}")
+
+          begin
+            title = JSON.parse(queue_data)["name"]
+            count = @redis.lpush("mups:titles", title)
+
+            mtime = JSON.parse(queue_data)["mtime"]
+            @redis.lpush("mups:mtime", mtime)
+
+            @logger.info("REDIS -> list count #{count}")
+          rescue JSON::ParserError => e
+            @logger.error(e.message)
+          end
         end
       end
 
