8b17bb0016a3b60d355027ba764a345c1c3b9860
bronzdoc <lsagastume1990@gmail.com>
2016-06-26 23:55:35 -0600

    Adding help message

--- lib/mups.rb
+++ lib/mups.rb
@@ -1,9 +1,36 @@
 require "mups/version"
+require "mups/redis"
 require "mups/consumer"
+require "logger"
 
 module Mups
-  def self.start
-    consumer = Mups::Consumer.new
-    consumer.start
+  def self.start(redis_url=nil)
+    if redis_url.nil?
+      help
+      exit
+    end
+
+    logger = Logger.new(STDOUT)
+    begin
+      redis = Mups::Redis.new(redis_url)
+      Mups::Consumer.new(redis).start
+    rescue ::Redis::CannotConnectError
+      logger.info("Could not connect to #{redis_url}")
+    end
+  end
+
+  def self.help
+    puts <<-HELP
+    Mups will connect to the Meetup.com event stream and store event titles in redis
+    REDIS list name: mups:titles
+
+    Usage:
+     gem -h/--help
+     gem url
+
+    Examples:
+     mups redis://user@server:6379
+    HELP
+
   end
 end
